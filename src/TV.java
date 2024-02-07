7713392d3237f2dc9695f369dd5a3ee1ec095513
Mahi <ahnafraismahi@gmail.com>
2024-02-07 19:34:22 +0600

    TV Interface created

---
+++ src/TV.java
@@ -0,0 +1,19 @@
+public interface TV {
+    boolean isEnabled();
+
+    void enable();
+
+    void disable();
+
+
+    int getVolume();
+
+    void setVolume(int vol);
+
+
+    int getChannel();
+
+
+    void setChannel(int channel);
+
+}
